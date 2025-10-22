9087dab40c82f14ccd7d5806fbaae4f63195a0f3
veggiemania <milkcowgames@gmail.com>
2025-10-22 16:07:56 +0200

    Duplicado el escenario para implementar la lógica del cambio de cámaras y luces; con el botón Escape se muestra u oculta el menú

---
+++ Assets/Scripts/CementerioCambioCamaras/UIController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject canvas;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeSelf)
+                canvas.gameObject.SetActive(false);
+            else
+                canvas.gameObject.SetActive(true);
+        }
+    }
+}
