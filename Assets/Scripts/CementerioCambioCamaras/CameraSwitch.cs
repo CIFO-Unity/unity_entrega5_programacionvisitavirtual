3d4878641a65d5804f4cc8111baa7b01d239b035
veggiemania <milkcowgames@gmail.com>
2025-10-23 18:20:27 +0200

    Modificado el script CameraSwitch para dejar seleccionado el botón al hacer click

--- Assets/Scripts/CementerioCambioCamaras/CameraSwitch.cs
+++ Assets/Scripts/CementerioCambioCamaras/CameraSwitch.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CameraSwitch : MonoBehaviour
 {
     [Header("Cámara a Activar")]
     [Tooltip("La cámara que se activará al hacer clic en el botón")]
     [SerializeField] private Camera targetCamera;
+
+    private Button boton;
+
+    void Awake()
+    {
+        boton = GetComponent<Button>();
+    }
+
+    void Start()
+    {
+        if (targetCamera != null && targetCamera.enabled)
+        {
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+    }
 
     public void ActivateThisCamera()
     {
@@ -14,15 +31,17 @@ public class CameraSwitch : MonoBehaviour
             return;
         }
 
-        // Desactivar todas las cámaras de la escena
+        // 🔸 Desactivar todas las cámaras
         Camera[] allCameras = FindObjectsByType<Camera>(FindObjectsSortMode.None);
         foreach (Camera cam in allCameras)
         {
             cam.enabled = false;
         }
-        
-        // Activar solo la cámara objetivo
+
+        // 🔸 Activar solo la cámara objetivo
         targetCamera.enabled = true;
-        
+
+        // 🔸 Marcar este botón como seleccionado → se aplicará el “Selected Color”
+        EventSystem.current.SetSelectedGameObject(gameObject);
     }
 }
